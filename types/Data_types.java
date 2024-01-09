c964fb70275db254fa3e979f5a124311bdb0eb52
Valentin Zadorozhniy <Zadorozhnaya@mail.ru>
2024-01-09 08:40:05 +0700

    data_types-v1

---
+++ types/Data_types.java
@@ -0,0 +1,25 @@
+package types;
+
+public class Data_types {
+  public static void main(String[] args) {
+    //Primitive data types
+    int a = 10;
+    byte b = 10;
+    short c = 10;
+    long d = 10L;
+    float e = 10.0f;
+    double f = 10.0;
+    boolean j = true;
+    char h = 'A';
+
+    System.out.println(a + " " + b + " " + d  + " " + c + " " + d + " " + e + " " + f + " " + j + " " + h);
+
+    //Reference data types
+    String k = "Hello";
+    System.out.println(k);
+
+    //Array data types
+    int[] l = {10, 20, 30};
+    System.out.println(l);
+  }
+}
