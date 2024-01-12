c810abb66b7a9f9db8bcfd764c7db458d08b0b09
Valentin Zadorozhniy <Zadorozhnaya@mail.ru>
2024-01-12 08:32:18 +0700

    add classes

--- Classes/Car.java
+++ Classes/Car.java
@@ -1,7 +1,25 @@
 package Classes;
 
 public class Car extends AbstractCar implements InnerCar {
+  int doors;
+  int whiles;
+  String brand;
+  Car(int doors, int whiles, String brand) {
+    this.doors = doors;
+    this.whiles = whiles;
+    this.brand = brand;
+  }
+ 
+  Car(){
+    this(4, 4, "Haval");
+  }
   public String drive() {
     return "vroom";
 }
+}
+
+class SubCar extends Car {
+  SubCar(){
+    super(4, 4, "Haval");
+  }
 }
--- Classes/Classes.java
+++ Classes/Classes.java
@@ -5,5 +5,10 @@ public class Classes {
     Car car = new Car();
     System.out.println(car.drive());
     System.out.println(car.honk());
+
+    SubCar subCar = new SubCar();
+    System.out.println(subCar.drive());
+    System.out.println(subCar.honk());
+
   }
 }
